4d9de92deb5626d64f57259fe939a4a287ca23c6
saicharanbm <saicharanbm@gmail.com>
2025-01-06 23:10:27 +0530

    add CORS support and update scripts in API

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,5 +2,12 @@ import express from "express";
 import "dotenv/config";
 import multer from "multer";
 import path from "path";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is listening to port : ${process.env.PORT || 3000}`);
+});
