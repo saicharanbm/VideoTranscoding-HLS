a53c8d92db2e4ebe6edf112549d107feb59437af
saicharanbm <saicharanbm@gmail.com>
2025-01-07 00:45:26 +0530

    implement file upload and transcoding functionality in API

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,11 +3,54 @@ import "dotenv/config";
 import multer from "multer";
 import path from "path";
 import cors from "cors";
+import client from "./db";
 
 const app = express();
 
 app.use(cors());
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "..", "/uploads"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const fileName = file.originalname.split(".");
+    cb(null, fileName[0] + "-" + uniqueSuffix + "." + fileName[1]);
+  },
+});
+const upload = multer({ storage });
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is listening to port : ${process.env.PORT || 3000}`);
+});
+
+app.put("/upload", upload.single("file"), async (req, res) => {
+  const { file } = req;
+  if (!file) {
+    res.status(400).json({ message: "Upload failed" });
+    return;
+  }
+  const newUpload = await client.upload.create({
+    data: {
+      uploadUrl: path.join(__dirname, "uploads", file?.filename),
+    },
+  });
+  res.json({ message: "Upload successful", uploadId: newUpload.id });
+});
+
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const uploadFromDb = await client.upload.findFirst({
+    where: { id },
+  });
+
+  if (!uploadFromDb) {
+    res.status(404).json({ message: "File not found" });
+    return;
+  }
+
+  res.json({
+    status: uploadFromDb.status.toString(),
+  });
 });
--- api/src/worker.ts
+++ api/src/worker.ts
@@ -1,1 +1,65 @@
 import { Status, Upload } from "@prisma/client";
+import { exec } from "child_process";
+import db from "./db";
+import { mkdir } from "fs";
+import path from "path";
+
+let isTranscoding = false;
+function Transcode(upload: Upload) {
+  isTranscoding = true;
+  //create a new promise here
+  const baseOutPath = path.join(__dirname, "..", "uploads", upload.id, "360p");
+  mkdir(baseOutPath, (err) => {
+    const command = `ffmpeg -i ${upload.uploadUrl} -s 640x360 -c:v h264 -b:v 800k -c:a aac -b:a 128k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "${baseOutPath}/360p_%03d.ts" ${baseOutPath}/360p.m3u8`;
+    // -map [v480out] -map 0:a -c:v h264 -b:v:1 1400k -c:a aac -b:a:1 128k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "/480p/480p_%03d.ts" 480p.m3u8 \
+    // -map [v720out] -map 0:a -c:v h264 -b:v:2 2800k -c:a aac -b:a:2 192k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "/720p/720p_%03d.ts" 720p.m3u8 \
+    // -map [v1080out] -map 0:a -c:v h264 -b:v:3 5000k -c:a aac -b:a:3 192k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "/1080p/1080p_%03d.ts" 1080p.m3u8`;
+
+    exec(command, async (err, stdout, stderr) => {
+      //Resolve it here
+      console.log(stdout);
+      console.log(stderr);
+      if (err) {
+        await new Promise((r) => setTimeout(r, 1000));
+        return;
+      }
+      await db.upload.update({
+        where: {
+          id: upload.id,
+        },
+        data: {
+          status: Status.processed,
+        },
+      });
+      isTranscoding = false;
+    });
+  });
+}
+
+async function main() {
+  while (true) {
+    const upload = await db.upload.findFirst({
+      where: {
+        status: "processed",
+      },
+    });
+
+    console.log({ upload, isTranscoding });
+    if (!upload || isTranscoding) {
+      await new Promise((r) => setTimeout(r, 1000));
+      continue;
+    }
+
+    await db.upload.update({
+      where: {
+        id: upload.id,
+      },
+      data: {
+        status: Status.processed,
+      },
+    });
+    Transcode(upload);
+  }
+}
+
+main();
